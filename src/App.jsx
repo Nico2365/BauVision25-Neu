42226f34ccc2d026434a8ac64cd94599fad97aa0
Nico2365 <nico.gruendler@gmx.de>
2025-07-09 16:07:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Willkommen bei BauVision25</h1>
-      <p>Die App wurde erfolgreich geladen.</p>
+      <p>Die App funktioniert jetzt!</p>
     </div>
   );
 }
-
-export default App;
